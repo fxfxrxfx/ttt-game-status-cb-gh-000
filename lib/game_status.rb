841aec5282ccf94702488ced08729f2a701188bc
Santiago Betancur <santi-c-b@outlook.com>
2018-11-26 02:51:39 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,10 @@ WIN_COMBINATIONS = [
 
 def full?(board)
   board.each do |cell|
-    if(cell == " ") return false
+    if(cell == " ")
+      return false
+    end
+  return true
 end
 
 def draw?(board)
