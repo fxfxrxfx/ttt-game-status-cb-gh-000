72b4ff6018cec344821f845909319e7e23e2c216
Santiago Betancur <santi-c-b@outlook.com>
2018-11-26 03:01:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -43,10 +43,6 @@ def won?(board)
 
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") then
       return win_combination # return the win_combination indexes that won.
-    elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
-      return win_combination # return the win_combination indexes that won.
-    else
-      false
     end
   end
   return false
