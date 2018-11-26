63e4e1618971097ebf9c2637ee34d4bb0defa189
Santiago Betancur <santi-c-b@outlook.com>
2018-11-26 03:05:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,19 +31,7 @@ end
 def won?(board)
   if !full?(board) then return false end
   WIN_COMBINATIONS.each do |win_combination|
-    # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-    # grab each index from the win_combination that composes a win.
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1] # load the value of the board at win_index_1
-    position_2 = board[win_index_2] # load the value of the board at win_index_2
-    position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X") then
-      return win_combination # return the win_combination indexes that won.
-    end
+    puts win_combination
   end
   return nil
 end
