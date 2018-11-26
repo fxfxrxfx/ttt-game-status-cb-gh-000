2e22e3bf762368aeb9357c08d3817799fe09ac1d
Santiago Betancur <santi-c-b@outlook.com>
2018-11-26 02:42:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,48 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+
+def full?(board)
+  board.each do |cell|
+    if cell == " " then
+      return false
+    end
+  end
+  return true
+end
+
+def draw?(board)
+  return full?(board) && !won?(board)
+end
+
+def won?(board)
+  9.each do |i|
+    if position_taken(board, i) then return nil end
+  WIN_COMBINATIONS.each do |win_combination|
+    # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+    # grab each index from the win_combination that composes a win.
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination # return the win_combination indexes that won.
+    else
+      false
+    end
+  end
+end
