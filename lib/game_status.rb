0950e5d9bd4638fb4422cc595df3c24912b1e6e7
Santiago Betancur <santi-c-b@outlook.com>
2018-11-26 03:08:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,7 +42,7 @@ def won?(board)
     position_3 = board[win_index_3] # load the value of the board at win_index_3
 
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") then
-      puts "#{win_index_3}, #{win_index_2}, #{win_index_1}"
+      puts "EL JUEGO SE GANÓ ASÍ #{win_index_3}, #{win_index_2}, #{win_index_1}"
       return win_combination # return the win_combination indexes that won.
     end
   end
